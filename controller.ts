25ef2fe8c01c9a60bbec0e605370abeb10251d43
Michel de Bree <michel@micheldebree.nl>
2017-04-19 20:49:46 +0200

    Send picture in response. Slow though.

--- controller.ts
+++ controller.ts
@@ -1,6 +1,19 @@
 import * as express from 'express';
+import * as jimp from 'jimp';
 export const router = express.Router();
 
 router.get('/', function(req, res) {
-    res.status(200).send('Hello World!');
+  try {
+    jimp.read('test.jpg', (err, image) => {
+      if (err) throw err;
+      image.getBuffer(jimp.AUTO, (err, buffer) => {
+        if (err) throw err;
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.status(200).send(buffer);
+      });
+    });
+  }
+  catch (e) {
+    res.status(500).send(e);
+  }
 });
